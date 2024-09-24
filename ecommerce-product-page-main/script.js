40b8d9206f2a95ef1ef64446ecdd42a63971f53d
joykara <joykarani1@gmail.com>
2024-09-24 20:03:02 +0300

    chore: set up hamburger menu display

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,7 +1,12 @@
 // toggle hamburger menu
 const toggleMenu = document.getElementById('hamburger-menu');
 const toggle = document.getElementById('toggle');
+const closeMenu = document.getElementById('close');
 
 toggle.addEventListener('click', () => {
     toggleMenu.classList.toggle('show');
 })
+
+closeMenu.addEventListener('click', () => {
+    toggleMenu.classList.remove('show');
+});
